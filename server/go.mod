da2a1b6b61c0dc602c76eb49c191d6b6ac4b717d
Damien Nicolleau <damien.nicolleau@energisme.com>
2024-10-27 18:21:46 +0100

    Added user creation with password hash

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module server
 
 go 1.23.2
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.28.0
+)
--- server/internal/user/user_service.go
+++ server/internal/user/user_service.go
@@ -1,1 +1,50 @@
 package user
+
+import (
+	"context"
+	"server/util"
+	"strconv"
+	"time"
+)
+
+type service struct {
+	timeout time.Duration
+	Repository
+}
+
+func NewService(repository Repository) Service {
+	return &service{
+		time.Duration(2) * time.Second,
+		repository,
+	}
+}
+
+func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+
+	// TODO : hash password
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	u := &User{
+		Username: req.Username,
+		Email:    req.Email,
+		Password: hashedPassword,
+	}
+
+	r, err := s.Repository.CreayteUser(ctx, u)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &CreateUserRes{
+		ID:       strconv.Itoa(int(r.ID)),
+		Username: r.Username,
+		Email:    r.Email,
+	}
+
+	return res, nil
+}
